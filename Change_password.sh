756a31eb4077867e2fb24a2256ba12b5baf9b576
ashwinkoduvathara <66616950+ashwinkoduvathara@users.noreply.github.com>
2022-06-27 22:05:38 +0530

    Update Change_password.sh

--- Change_password.sh
+++ Change_password.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+
+if [ "$EUID" -ne 0 ]
+          then echo "Please run as root"
+                    exit
+fi
+
+echo "Please enter Username:"
+read username
+
+echo "Enter the new Password:"
+read -s password1
+
+echo "Confirm the Password:"
+read -s password2
+
+# Check both passwords match
+if [ $password1 != $password2 ]; then
+            echo "Passwords do not match"
+                 exit
+fi
+
+
+
+echo -e "$password1\n$password1" | passwd $username                                                                                                                     ~                                                                                                                       ~                                                                                                                       ~                                                          
