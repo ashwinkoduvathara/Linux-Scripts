68c8acea6cb045fb4cf33ec3a0bbfcc27f37c706
ashwinkoduvathara <66616950+ashwinkoduvathara@users.noreply.github.com>
2022-09-22 06:54:35 +0100

    Update webpanel.sh
    outline created

--- webpanel.sh
+++ webpanel.sh
@@ -8,3 +8,18 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
+
+
+#Finding the OS Version
+
+#Uninstall packages
+
+
+#install LAMP
+
+
+#configure firewall
+
+
+#PREPARING THE SERVER
+
