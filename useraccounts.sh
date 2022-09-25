9e5547e6d9a598ffcb93aee934433244e4176931
ashwinkoduvathara <66616950+ashwinkoduvathara@users.noreply.github.com>
2022-09-25 14:52:54 +0530

    Create useraccounts.sh

---
+++ useraccounts.sh
@@ -0,0 +1,16 @@
+if [[ $EUID -ne 0 ]]; then
+   echo -e " This script must be run as root "
+   exit 1
+fi
+
+username=$(cat /tmp/userlist | tr 'A-Z'  'a-z')
+
+for user in $username
+do
+       useradd $user >> ~/Downloads/user_key.txt
+       password=$(</dev/urandom tr -dc A-Za-z0-9 | head -c16)
+       echo password of $user is $password >>~/Downloads/user_key.txt
+       echo -e "$password\n$password" | passwd $user
+       usermod -aG sudo $user
+
+done
