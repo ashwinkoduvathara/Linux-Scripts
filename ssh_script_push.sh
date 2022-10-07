ed167f02ffe8b90f95d1b82a30d25b5c55ccc735
ashwinkoduvathara <ashwinkoduvathara@gmail.com>
2022-10-07 17:11:23 +0530

    package installation

--- ssh_script_push.sh
+++ ssh_script_push.sh
@@ -9,13 +9,32 @@ success="\033[1;32m"
 warning="\033[1;33m"
 error="\033[1;31m"
 nocolour="\033[00m"
-
+user="root"
+password="asd123."
 
 
 if [[ $EUID -ne 0 ]]; then
    echo -e " $error This script must be run as root $nocolour "
    exit 1
 fi
+
+
+echo "$warning powered by $nocolour ";
+$success
+echo "----    ----   --------   ----------   ----    ---- ";
+echo "****   ****   **********  ************ ****    **** ";
+echo "----  ----   ----    ---- --        -- ----    ---- ";
+echo "*********    ***      *** **        ** ****    **** ";
+echo "---------    ---      --- --        -- ----    ---- ";
+echo "****  ****   ****    **** **        ** ************ ";
+echo "----   ----   ----------  ------------ ------------ ";
+echo "****    ****   ********   **********   ************ ";
+echo "                                                    ";
+$nocolour
+
+
+
+
 
 
 #finding the package manager
@@ -31,37 +50,27 @@ for f in ${!osInfo[@]}
 do
     if [[ -f $f ]];then
         Package_manager=${osInfo[$f]}
-        
+        $Package_manager install sshpass fping nmap -y
        
     fi
 done
 
-if [[ $Package_manager -eq "yum" ]]; then
-   $Package_manager install sshpass -y
+#--------------------------------------------------------------------tested upto here
 
-   
-fi
-
-if [[ $Package_manager -eq "apt-get" ]]; then
-   $Package_manager install sshpass -y
-
-   
-fi
-
-
-user="root"
 declare -a host= ("192.168.100.212" "192.168.100.213")
-
 script_path=""
 
+#for client in "${client_machine[@]}"
+#do
 
 
-for client in "${client_machine[@]}"
-do
+#sshpass -p "asd123." ssh $user $host
 
-ssh -l $user $host 'bash -s' < $script_path
+curl --insecure --user $user:$password -T /root/ip.txt  sftp://$host/root/     #working         
 
-done
+#ssh -l $user $host 'bash -s' < $script_path
+
+#done
 
 
 
